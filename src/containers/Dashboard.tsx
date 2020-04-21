c85f59b585cde5ee9375b062c4f8606edc4262b5
renso3x <enso123456@gmail.com>
2020-04-21 17:31:09 +0800

    display 14 days selected from calendar

--- src/containers/Dashboard.tsx
+++ src/containers/Dashboard.tsx
@@ -1,6 +1,52 @@
 import React from 'react';
+import DayPicker from 'react-day-picker';
+import moment from 'moment';
+import _ from 'lodash';
 
-export const Dashboard: React.FC = (): JSX.Element => {
-  return <h1>Welcome to Dashboard</h1>
+export class Dashboard extends React.Component {
+  state = {
+    selectedDay: new Date(),
+    days: [],
+    daysInMonth: []
+  }
+
+  handleDayClick = (day: Date) => {
+    this.setState({
+      selectedDay: day,
+    });
+  }
+
+  renderDates() {
+    return (
+      <ul>
+        {
+          _.times(14, (n: number) =>  {
+            return(
+              <li style={{ display: 'inline', margin: 5 }} key={n}>{moment(this.state.selectedDay, "DD-MM-YYYY").add(n, 'days').format('dd D - MMM')}</li>
+            )
+          })
+        }
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Dashboard</h1>
+        <DayPicker
+          selectedDays={this.state.selectedDay}
+          onDayClick={this.handleDayClick}
+        />
+
+        <p>
+          {this.state.selectedDay
+            ? this.state.selectedDay.toLocaleDateString()
+            : 'Please select a day 👻'}
+        </p>
+
+        {this.renderDates()}
+      </div>
+    )
+  }
 }
-
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'react-calendar/dist/Calendar.css';
+import 'react-day-picker/lib/style.css';
+
 import { store } from './config';
 
 import { RootRouter } from './containers';
